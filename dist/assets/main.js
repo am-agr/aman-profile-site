41e2c70d945d2446b03744d798be5882bca9eed3
Aman Agrawal <agrawalamanhnd@gmail.com>
2025-07-17 02:10:38 +0530

    Initial commit

---
+++ dist/assets/main.js
@@ -0,0 +1,1 @@
+console.log("Aman Agrawal Portfolio - Production Build Loaded");
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/aman-profile-site/',
+  base: "/2/",
   plugins: [react()],
-})
+});
